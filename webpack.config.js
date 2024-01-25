3e7aadc67da48289398ffa48762c566f666514cf
Angelica <Angelica137@users.noreply.github.com>
2024-01-25 18:56:27 +0000

    configure babel

--- webpack.config.js
+++ webpack.config.js
@@ -2,4 +2,19 @@ const path = require("path");
 const webpack = require("webpack");
 module.exports = {
   entry: "./src/client/index.js",
+
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          iotions: {
+            presets: ["@babel/preset-env"],
+          },
+        },
+      },
+    ],
+  },
 };
