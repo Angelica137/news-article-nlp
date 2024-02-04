fa615bdf17bd081413c441a86bbb28658eaf7a4a
Angelica <Angelica137@users.noreply.github.com>
2024-02-04 10:54:42 -0600

    complete splitting environments

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,41 +1,6 @@
-const path = require("path");
-const webpack = require("webpack");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
-const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const { merge } = require("webpack-merge");
+const common = require("./webpack.config.js");
 
-module.exports = {
-  entry: "./src/client/index.js",
+module.exports = merge(common, {
   mode: "production",
-
-  module: {
-    rules: [
-      {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        use: {
-          loader: "babel-loader",
-          options: {
-            presets: ["@babel/preset-env"],
-          },
-        },
-      },
-      // add scss loaders
-      {
-        test: /\.scss$/,
-        use: ["style-loader", "css-loader", "sass-loader"],
-      },
-    ],
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: "src/client/views/index.html",
-      filename: "index.html",
-    }),
-    new CleanWebpackPlugin({
-      dry: true,
-      verbose: true,
-      cleanStaleWebpackAssets: true,
-      protectWebpackAssets: false,
-    }),
-  ],
-};
+});
