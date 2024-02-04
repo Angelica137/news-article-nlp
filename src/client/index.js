f3505dfc9d7e006caa212fbc37f74267a4f25364
Angelica <Angelica137@users.noreply.github.com>
2024-02-04 12:31:01 -0600

    add reposne to post request to the client side

--- src/client/index.js
+++ src/client/index.js
@@ -12,3 +12,16 @@ import "./styles/base.scss";
 import "./styles/footer.scss";
 import "./styles/form.scss";
 import "./styles/header.scss";
+
+fetch("/summarise", {
+  method: "POST",
+  headers: {
+    "Content-Type": "application/json",
+  },
+  body: JSON.stringify({ url: "http://example.com/article" }),
+})
+  .then((response) => response.json())
+  .then((data) => {
+    console.log("Summary:", data);
+  })
+  .catch((error) => console.error("Error:", error));
