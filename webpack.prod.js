6db644417fe48f7831201093fe4413df5c6c1692
Angelica <Angelica137@users.noreply.github.com>
2024-02-05 07:34:15 -0600

    add mini css extract plugin to webpack.prod

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,6 +1,26 @@
 const { merge } = require("webpack-merge");
 const common = require("./webpack.config.js");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports = merge(common, {
   mode: "production",
+  module: {
+    rules: [
+      {
+        test: /\.scss$/,
+        use: [
+          // replace "style-loader"
+          MiniCssExtractPlugin.loader,
+          "css-loader",
+          "sass-loader",
+        ],
+      },
+    ],
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: "[name].[contenthash].css",
+      chunkFilename: "[id].[contenthash].css",
+    }),
+  ],
 });
