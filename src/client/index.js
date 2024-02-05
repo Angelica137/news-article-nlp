3df53fdaa0c7091b9f75f3e35c3e6edf8c846954
Angelica <Angelica137@users.noreply.github.com>
2024-02-05 07:38:45 -0600

    remove commented out fetch call form index.js

--- src/client/index.js
+++ src/client/index.js
@@ -12,23 +12,3 @@ import "./styles/base.scss";
 import "./styles/footer.scss";
 import "./styles/form.scss";
 import "./styles/header.scss";
-
-/*
-fetch("/summarise", {
-  method: "POST",
-  headers: {
-    "Content-Type": "application/json",
-  },
-  body: JSON.stringify({ url: "http://example.com/article" }),
-})
-  .then((response) => {
-    if (!response.ok) {
-      throw new Error("Network response was not ok" + response.statusText);
-    }
-    return response.json();
-  })
-  .then((data) => {
-    console.log("Summary:", data);
-  })
-  .catch((error) => console.error("Error:", error));
-*/
