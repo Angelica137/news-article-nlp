b8be79eff46d8f2596b47411d82ca96c51619662
Angelica <Angelica137@users.noreply.github.com>
2024-02-05 12:59:02 -0600

    correctly serve the client main.js

--- src/server/index.js
+++ src/server/index.js
@@ -9,7 +9,17 @@ const app = express();
 
 app.use(express.json());
 
-app.use(express.static("dist"));
+app.use(express.static(path.join(__dirname, "../../dist")));
+
+// get the home page
+app.get("/", function (req, res) {
+  //console.log(__dirname);
+  res.sendFile(path.join(__dirname, "../../dist", "index.html"));
+  // use absolute path
+  /*const distPath = path.join(__dirname, "..", "..", "dist");
+  console.log(__dirname);
+  res.sendFile(path.join(distPath, "index.html"));*/
+});
 
 app.get("/summarise", (req, res) => {
   //console.log(__dirname);
