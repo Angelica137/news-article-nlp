865d915c86ac98802e37eeca4803c78c3a17f3bb
Angelica <Angelica137@users.noreply.github.com>
2024-02-05 09:41:47 -0600

    added back commented code

--- src/server/index.js
+++ src/server/index.js
@@ -8,6 +8,7 @@ const axios = require("axios");
 const app = express();
 
 app.use(express.json());
+
 app.use(express.static("dist"));
 
 console.log(__dirname);
@@ -15,6 +16,10 @@ console.log(__dirname);
 // this call gets the home page
 app.get("/", function (req, res) {
   res.sendFile(path.resolve(__dirname, "dist", "index.html"));
+});
+
+app.post("/summarise", (req, res) => {
+  res.json({ message: "Summarise route hit" });
 });
 
 app.listen(8080, function () {
@@ -30,6 +35,7 @@ app.post("/summarise", async (req, res) => {
 
   /* send post request to api sending the url to summarise from the input field
 	on the client side */
+
   try {
     const response = await axios.post(
       "https://api.meaningcloud.com/summarization-1.0",
