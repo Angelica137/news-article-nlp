0d8c71b43de0cf6ac5989b8004e167d53ab040b0
Angelica <Angelica137@users.noreply.github.com>
2024-02-07 09:00:03 -0600

    sumamrisation works, still unformatted

--- src/client/index.js
+++ src/client/index.js
@@ -15,3 +15,27 @@ import "./styles/base.scss";
 import "./styles/footer.scss";
 import "./styles/form.scss";
 import "./styles/header.scss";
+
+document.getElementById("button").addEventListener("click", async () => {
+  const urlToSummarise = document.getElementById("name").value;
+
+  try {
+    const response = await fetch("/submitForm", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ urlToSummarise }),
+    });
+    if (!response.ok) {
+      throw new Error(`HTTP error! Status: ${response.status}`);
+    }
+    const data = await response.json();
+
+    document.getElementById("results").innerHTML = data.summary;
+  } catch (error) {
+    console.log("Error", error);
+    document.getElementById("results").innerHTML =
+      "Error processing your request, please try again";
+  }
+});
