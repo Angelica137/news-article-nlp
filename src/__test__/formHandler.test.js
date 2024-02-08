ac6df41ed08fac905d55c3d48c290333b2296289
Angelica <Angelica137@users.noreply.github.com>
2024-02-08 08:53:54 -0600

    add jsdom

--- src/__test__/formHandler.test.js
+++ src/__test__/formHandler.test.js
@@ -1,4 +1,4 @@
-import { handleSubmit } from "../src/client/js/formHandler";
+import { handleSubmit } from "/Users/Angelica/Documents/Coding/Udacity/front-end-nanodegree/news-article-nlp/src/client/js/formHandler.js";
 
 // Mock the fetch function
 global.fetch = jest.fn(() =>
